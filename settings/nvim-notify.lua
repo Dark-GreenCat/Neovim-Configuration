88a0cc40ccae5ce9df06f78937114e26614e54ff
Dark-GreenCat <tranquanghuy3008@outlook.com>
2024-06-29 17:24:14 +0700

    [ADD] Forward lua print to nvim-notify

--- settings/nvim-notify.lua
+++ settings/nvim-notify.lua
@@ -4,9 +4,29 @@ notify.setup({
   render = "default"
 })
 
-vim.notify = notify
+vim.notify = require("notify")
 
 notify("Welcome to Neovim - the new-life text editor", "info", {
   title = "Have a good day, Mint!"
 })
 
+local ok, ntfy = pcall(require, "notify")
+if not ok then
+    return
+end
+ntfy.setup()
+if not old_print then
+    old_print = _G.print
+end
+vim.notify = function(msg, level, opts)
+    old_print(tostring(level) .. ": " .. msg .. " : " .. vim.inspect(opts))
+    ntfy(msg, level, opts)
+end
+print = function(...)
+    local print_safe_args = {}
+    local _ = { ... }
+    for i = 1, #_ do
+        table.insert(print_safe_args, tostring(_[i]))
+    end
+    vim.notify(table.concat(print_safe_args, ' '), "info")
+end
